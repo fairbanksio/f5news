c25cb0195768c78ed7a37fab29b4d66e82cfba48
Jon Fairbanks <fairplay89@gmail.com>
2020-09-01 12:36:09 -0700

    More Lint Fixes

--- client/index.js
+++ client/index.js
@@ -8,7 +8,7 @@ mongoose.Promise = Q.Promise;
 const app = express();
 const port = process.env.PORT || 3000;
 
-function connectToDB() {
+connectToDB = () => {
   mongoose.connect(
     process.env.MONGO_URI || 'mongodb://localhost/f5oclock',
     {
@@ -38,25 +38,25 @@ mongoose.connection.on('error', (err) => {
   setTimeout(() => { connectToDB(); }, 3000);
 });
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.resolve(__dirname, '/public')));
 app.engine('html', require('ejs').renderFile);
 
-app.get('/', function(_req, res) { res.render('index.html') });
-app.get('/getPosts', function(_req, res) {
+app.get('/', (_req, res) => { res.render('index.html') });
+app.get('/getPosts', (_req, res) => {
   const utcDate = Math.floor((new Date()).getTime() / 1000);
   // Depending on time per day 30 minute and 60 minute searches in database
-  const timeAdjust = function() {
+  const timeAdjust = () => {
     const today = new Date().getUTCHours();
     if (today >= 11 && today <= 23) {
       return '7200'; // 2 hours
     } else {
       return '14400'; // 4 hours
     }
-  }
+  };
   let searchTime = utcDate - timeAdjust();
   // Search the db and return up to 20 docs
   newPost
-    .find({ created_utc: { $gt: searchTime },  upvoteCount: { $gt: 5 }})
+    .find({ created_utc: { $gt: searchTime },  upvoteCount: { $gt: 5 } })
     .sort({ upvoteCount: -1, created_utc: 1 })
     .limit(20)
     .exec()
@@ -64,6 +64,6 @@ app.get('/getPosts', function(_req, res) {
     .catch(console.warn); // eslint-disable-line no-console
 });
 
-app.listen(port, function () {
-  console.log('f5 o\'clock running on port ' + port + '!'); // eslint-disable-line no-console
+app.listen(port, () => {
+  console.log(`f5 o\'clock running on port ${port}!`); // eslint-disable-line no-console
 });
