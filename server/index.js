c1a4c5559f7a5757c9eb8d25f47f6712f632cd63
Jon Fairbanks <fairplay89@gmail.com>
2020-09-18 23:24:17 -0700

    Log memory usage

--- server/index.js
+++ server/index.js
@@ -71,6 +71,7 @@ const fetchPosts = () => rp('https://www.reddit.com/r/politics/rising.json')
   .then(fetchPosts)
   .then(
     console.log(`Saved New Posts @ ${Date.now()}`), // eslint-disable-line no-console
+    console.log(`Currently using ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB of memory \n`)
   )
   .catch(() => {
     console.warn(`Error Fetching Posts @ ${Date.now()}. This may be due to a timeout from Reddit. F5 will try again shortly.`); // eslint-disable-line no-console
@@ -80,7 +81,7 @@ const fetchPosts = () => rp('https://www.reddit.com/r/politics/rising.json')
 connectToDB();
 
 mongoose.connection.on('connected', () => {
-  console.log('F5 is now saving posts to MongoDB...'); // eslint-disable-line no-console
+  console.log('F5 is now saving posts to MongoDB...\n'); // eslint-disable-line no-console
   wait(3).then(fetchPosts); // Start
 });
 
