b6ac6b7237565c9552329a626cca455478cffc7b
Jon Fairbanks <fairplay89@gmail.com>
2020-09-18 23:48:29 -0700

    Lint

--- server/index.js
+++ server/index.js
@@ -71,7 +71,7 @@ const fetchPosts = () => rp('https://www.reddit.com/r/politics/rising.json')
   .then(fetchPosts)
   .then(
     console.log(`Saved New Posts @ ${Date.now()}`), // eslint-disable-line no-console
-    console.log(`Currently using ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB of memory \n`) // eslint-disable-line no-console
+    console.log(`Currently using ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB of memory \n`), // eslint-disable-line no-console
   )
   .catch(() => {
     console.warn(`Error Fetching Posts @ ${Date.now()}. This may be due to a timeout from Reddit. F5 will try again shortly.`); // eslint-disable-line no-console
