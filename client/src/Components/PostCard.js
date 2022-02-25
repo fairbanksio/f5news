5b943347c7096f89dfab5849df82399723794329
bsord <brandon.sorgdrager@gmail.com>
2022-02-25 02:04:14 -0600

    initial video playback support

--- client/src/Components/PostCard.js
+++ client/src/Components/PostCard.js
@@ -46,56 +46,24 @@ export const PostCard = ({post}) => {
 
         <Modal onClose={onClose} isOpen={isOpen} isCentered >
           <ModalOverlay />
-          <ModalContent maxW='container.xl' maxH='100vh'>
-            <ModalCloseButton />
+          <ModalContent maxW='container.xl' maxH='100vh' bg='none'>
+            
             <ModalBody p={0}>
-
-              <Center>
-                <Image src={post.thumbnail === 'default' || post.thumbnail === 'self' || post.thumbnail === 'spoiler'? './placeholder.png' : post.thumbnail} w='100%' objectFit='contain' maxH='75vh' minW='50%'/>
-              </Center>
-              
-              <Box p={4} bg={hotnessBGColor(post.upvoteCount)}>
-                <Box display='flex' alignItems='baseline'>
+              <Center maxW='container.xl'>
+                <Box position='relative'>
                   <Box
-                    color='gray.500'
-                    fontWeight='semibold'
-                    letterSpacing='wide'
-                    fontSize='xs'
-                    textTransform='uppercase'
-                  >
-                    <Text href={'https://reddit.com/' + post.commentLink}  color='link'>
-                      {post.upvoteCount} upvotes &bull; {post.commentCount} comments &bull;  {timeAgoShort(post.created_utc)}
-                    </Text>
-                  </Box>
+                    as='video'
+                    controls
+                    loop="true" autoplay="autoplay"
+                    src={post.media.reddit_video.fallback_url}
+                    poster={post.thumbnail}
+                    objectFit='contain'
+                    type="application/x-mpegURL"
+                  />
+                  <ModalCloseButton />
                 </Box>
-
-                <Box
-                  mt='1'
-                  fontWeight='semibold'
-                  as='h4'
-                  lineHeight='tight'
-                  noOfLines={2}
-                >
-                  <Text href={post.url}>
-                    {post.title}
-                  </Text>
-                </Box>
-              </Box>
-
+              </Center>
             </ModalBody>
-            <ModalFooter >
-
-              <Stack direction={['row']} spacing={5}>
-                <Link href={'https://reddit.com/' + post.commentLink} isExternal color='link'>
-                  <Button>Comments</Button>
-                </Link>
-                
-                <Link href={post.url} isExternal>
-                  <Button>Visit</Button> 
-                </Link>
-              </Stack>
-
-            </ModalFooter>
           </ModalContent>
         </Modal>
       </Box>
@@ -486,6 +454,103 @@ export const PostCard = ({post}) => {
     </Box>
     
   )
+  else if (post.rpan_video) return (
+    <Box borderWidth='1px' borderRadius='lg' overflow='hidden' bg={hotnessBGColor(post.upvoteCount)}>
+      
+      <Box onClick={onOpen} >
+
+        <Box position='relative'>
+          <Image  src={post.thumbnail === 'default' || post.thumbnail === 'self' || post.thumbnail === 'spoiler'? './placeholder.png' : post.thumbnail} boxSize='100%' h='225px' objectFit='cover' position='relative'/>
+          <Box position='absolute' bottom='0' h='100%' color='white' width='100%' p={2} >
+            <Center h='100%' ><Text isTruncated fontSize='xs' bg='rgb(33,33,33,0.3)' background='black' p={5}>LIVE FEED</Text></Center>
+          </Box>
+        </Box>
+
+        <Box p={4} flex={1}>
+          <Box display='flex' alignItems='baseline' >
+            <Box
+              color='gray.500'
+              fontWeight='semibold'
+              letterSpacing='wide'
+              fontSize='xs'
+              textTransform='uppercase'
+            >
+              <Text href={'https://reddit.com/' + post.commentLink} color='link'>
+                {post.upvoteCount} upvotes &bull; {post.commentCount} comments &bull;  {timeAgoShort(post.created_utc)}
+              </Text>
+            </Box>
+          </Box>
+
+          <Box
+            mt='1'
+            fontWeight='semibold'
+            as='h4'
+            lineHeight='tight'
+            noOfLines={2}
+          >
+            <Text href={post.url} >
+              {post.title}
+            </Text>
+          </Box>
+        </Box>
+
+        <Modal onClose={onClose} isOpen={isOpen} isCentered >
+          <ModalOverlay />
+          <ModalContent maxW='container.xl' maxH='100vh'>
+            <ModalCloseButton />
+            <ModalBody p={0}>
+
+              <Center>
+                <Image src={post.thumbnail === 'default' || post.thumbnail === 'self' || post.thumbnail === 'spoiler'? './placeholder.png' : post.thumbnail} w='100%' objectFit='contain' maxH='75vh' minW='50%'/>
+              </Center>
+              
+              <Box p={4} bg={hotnessBGColor(post.upvoteCount)}>
+                <Box display='flex' alignItems='baseline'>
+                  <Box
+                    color='gray.500'
+                    fontWeight='semibold'
+                    letterSpacing='wide'
+                    fontSize='xs'
+                    textTransform='uppercase'
+                  >
+                    <Text href={'https://reddit.com/' + post.commentLink}  color='link'>
+                      {post.upvoteCount} upvotes &bull; {post.commentCount} comments &bull;  {timeAgoShort(post.created_utc)}
+                    </Text>
+                  </Box>
+                </Box>
+
+                <Box
+                  mt='1'
+                  fontWeight='semibold'
+                  as='h4'
+                  lineHeight='tight'
+                  noOfLines={2}
+                >
+                  <Text href={post.url}>
+                    {post.title}
+                  </Text>
+                </Box>
+              </Box>
+
+            </ModalBody>
+            <ModalFooter >
+
+              <Stack direction={['row']} spacing={5}>
+                <Link href={'https://reddit.com/' + post.commentLink} isExternal color='link'>
+                  <Button>Comments</Button>
+                </Link>
+                
+                <Link href={post.url} isExternal>
+                  <Button>Visit</Button> 
+                </Link>
+              </Stack>
+
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+      </Box>
+    </Box>
+  )
   else return (
     <Box borderWidth='1px' borderRadius='lg' overflow='hidden' bg={hotnessBGColor(post.upvoteCount)}>
       
@@ -494,7 +559,7 @@ export const PostCard = ({post}) => {
         <Box position='relative'>
           <Image  src={post.thumbnail === 'default' || post.thumbnail === 'self' || post.thumbnail === 'spoiler'? './placeholder.png' : post.thumbnail} boxSize='100%' h='225px' objectFit='cover' position='relative'/>
           <Box position='absolute' bottom='0' color='white' background='black' width='100%' p={2} bg='rgb(33,33,33,0.3)'>
-            <Text isTruncated fontSize='xs'> DEFAULT / UNDETERMINED </Text>
+            <Text isTruncated fontSize='xs'> Live video </Text>
           </Box>
         </Box>
 
