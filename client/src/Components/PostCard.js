8ee36d8b38c665e4adb1c7199b8d5a6a5a3ebcd7
bsord <brandon.sorgdrager@gmail.com>
2022-02-28 10:31:47 -0600

    Fix modal popping up when it shouldnt

--- client/src/Components/PostCard.js
+++ client/src/Components/PostCard.js
@@ -15,119 +15,114 @@ export const PostCard = ({post}) => {
 
   if (post.is_video) return (
     <Box borderWidth='1px' borderRadius='lg' overflow='hidden' bg={hotnessBGColor(post.upvoteCount)}>
-      
       <Box onClick={onOpen} >
-
         <Box position='relative'>
           <Image  src={post.thumbnail === 'default' || post.thumbnail === 'self' || post.thumbnail === 'spoiler'? './placeholder.png' : post.thumbnail} boxSize='100%' h='225px' objectFit='cover' position='relative'/>
           <Box position='absolute' top='0' right='0' color='white' p={2} color='white' background='black' bg='rgb(33,33,33,0.5)' borderRadius='full' m={2}>
             <Center h='100%' ><Icon fontSize='xl' as={FaVideo} /></Center>
           </Box>
         </Box>
-
-        <Box p={4} flex={1}>
-          <Box display='flex' alignItems='baseline' >
-            <Box
-              color='gray.500'
-              fontWeight='semibold'
-              letterSpacing='wide'
-              fontSize='xs'
-              textTransform='uppercase'
-            >
-              <Link href={'https://reddit.com' + post.commentLink} color='link' isExternal>
-                {post.upvoteCount} upvotes &bull; {post.commentCount} comments &bull;  {timeAgoShort(post.created_utc)}
-              </Link>
-            </Box>
-          </Box>
-
+      </Box>
+      <Box p={4} flex={1}>
+        <Box display='flex' alignItems='baseline' >
           <Box
-            mt='1'
+            color='gray.500'
             fontWeight='semibold'
-            as='h4'
-            lineHeight='tight'
-            noOfLines={2}
+            letterSpacing='wide'
+            fontSize='xs'
+            textTransform='uppercase'
           >
-            <Link href={post.url} isExternal>
-              {post.title.replace(/amp;/g,'')}
+            <Link href={'https://reddit.com' + post.commentLink} color='link' isExternal>
+              {post.upvoteCount} upvotes &bull; {post.commentCount} comments &bull;  {timeAgoShort(post.created_utc)}
             </Link>
           </Box>
         </Box>
 
-        <Modal onClose={onClose} isOpen={isOpen} isCentered blockScrollOnMount={false}>
-          <ModalOverlay />
-          <ModalContent maxW='container.xl' maxH='80vh' bg='none' w='auto'>
-            <ModalBody p={0}>
-              <Center >
-                <Box position='relative' width='100%' height='80vh' bg='#222'>
-                  <ReactPlayer url={post.media.reddit_video.dash_url} width='100%' height='100%' controls stopOnUnmount={false} playing/>
-                  
-                  <ModalCloseButton />
-                </Box>
-              </Center>
-            </ModalBody>
-          </ModalContent>
-        </Modal>
+        <Box
+          mt='1'
+          fontWeight='semibold'
+          as='h4'
+          lineHeight='tight'
+          noOfLines={2}
+        >
+          <Link href={post.url} isExternal>
+            {post.title.replace(/amp;/g,'')}
+          </Link>
+        </Box>
       </Box>
+
+      <Modal onClose={onClose} isOpen={isOpen} isCentered blockScrollOnMount={false}>
+        <ModalOverlay />
+        <ModalContent maxW='container.xl' maxH='80vh' bg='none' w='auto'>
+          <ModalBody p={0}>
+            <Center >
+              <Box position='relative' width='100%' height='80vh' bg='#222'>
+                <ReactPlayer url={post.media.reddit_video.dash_url} width='100%' height='100%' controls stopOnUnmount={false} playing/>
+                
+                <ModalCloseButton />
+              </Box>
+            </Center>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+      
     </Box>
   )
   else if (post.is_gallery) return (
     <Box borderWidth='1px' borderRadius='lg' overflow='hidden' bg={hotnessBGColor(post.upvoteCount)}>
-      
       <Box onClick={onOpen} >
-
         <Box position='relative'>
           <Image  src={post.thumbnail === 'default' || post.thumbnail === 'self' || post.thumbnail === 'spoiler'? './placeholder.png' : post.thumbnail} boxSize='100%' h='225px' objectFit='cover' position='relative'/>
           <Box position='absolute' top='0' right='0' color='white' p={2} color='white' background='black' bg='rgb(33,33,33,0.5)' borderRadius='full' m={2}>
             <Center h='100%' ><Icon fontSize='xl' as={FaImages} /></Center>
           </Box>
         </Box>
-
-        <Box p={4} flex={1}>
-          <Box display='flex' alignItems='baseline' >
-            <Box
-              color='gray.500'
-              fontWeight='semibold'
-              letterSpacing='wide'
-              fontSize='xs'
-              textTransform='uppercase'
-            >
-              <Link href={'https://reddit.com' + post.commentLink} color='link' isExternal>
-                {post.upvoteCount} upvotes &bull; {post.commentCount} comments &bull;  {timeAgoShort(post.created_utc)}
-              </Link>
-            </Box>
-          </Box>
-
+      </Box>
+      <Box p={4} flex={1}>
+        <Box display='flex' alignItems='baseline' >
           <Box
-            mt='1'
+            color='gray.500'
             fontWeight='semibold'
-            as='h4'
-            lineHeight='tight'
-            noOfLines={2}
+            letterSpacing='wide'
+            fontSize='xs'
+            textTransform='uppercase'
           >
-            <Link href={post.url} isExternal>
-              {post.title.replace(/amp;/g,'')}
+            <Link href={'https://reddit.com' + post.commentLink} color='link' isExternal>
+              {post.upvoteCount} upvotes &bull; {post.commentCount} comments &bull;  {timeAgoShort(post.created_utc)}
             </Link>
           </Box>
         </Box>
-        
-        <Modal onClose={onClose} isOpen={isOpen} isCentered >
-          <ModalOverlay />
-          <ModalContent maxW='container.xl' bg='none' w='auto' >
-            <ModalBody p={0}>
-              <Center maxW='container.xl' position='relative' overflow='hidden'>
-                <Stack overflowY='scroll' maxH='80vh' >
-                  {Object.keys(post.media_metadata).map((key) =>{
-                    return (
-                      <Image key={key} src={post.media_metadata[key].s.u.replace(/amp;/g,'')} w='100%' objectFit='cover' maxH='75vh' minW='50%'/>
-                    )
-                  })}
-                </Stack>
-                <ModalCloseButton color='white' mr={4}/>
-              </Center>
-            </ModalBody>
-          </ModalContent>
-        </Modal>
+
+        <Box
+          mt='1'
+          fontWeight='semibold'
+          as='h4'
+          lineHeight='tight'
+          noOfLines={2}
+        >
+          <Link href={post.url} isExternal>
+            {post.title.replace(/amp;/g,'')}
+          </Link>
+        </Box>
       </Box>
+      
+      <Modal onClose={onClose} isOpen={isOpen} isCentered >
+        <ModalOverlay />
+        <ModalContent maxW='container.xl' bg='none' w='auto' >
+          <ModalBody p={0}>
+            <Center maxW='container.xl' position='relative' overflow='hidden'>
+              <Stack overflowY='scroll' maxH='80vh' >
+                {Object.keys(post.media_metadata).map((key) =>{
+                  return (
+                    <Image key={key} src={post.media_metadata[key].s.u.replace(/amp;/g,'')} w='100%' objectFit='cover' maxH='75vh' minW='50%'/>
+                  )
+                })}
+              </Stack>
+              <ModalCloseButton color='white' mr={4}/>
+            </Center>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
     </Box>
     
   )
@@ -374,59 +369,57 @@ export const PostCard = ({post}) => {
   )
   else if (post.rpan_video) return (
     <Box borderWidth='1px' borderRadius='lg' overflow='hidden' bg={hotnessBGColor(post.upvoteCount)}>
-      
       <Box onClick={onOpen} >
-
         <Box position='relative'>
           <Image  src={post.thumbnail === 'default' || post.thumbnail === 'self' || post.thumbnail === 'spoiler'? './placeholder.png' : post.thumbnail} boxSize='100%' h='225px' objectFit='cover' position='relative'/>
           <Box position='absolute' top='0' right='0' color='white' p={2} color='white' background='black' bg='rgb(33,33,33,0.5)' borderRadius='full' m={2}>
             <Center h='100%' ><Icon fontSize='xl' as={FaVideo} color='red'/></Center>
           </Box>
         </Box>
-
-        <Box p={4} flex={1}>
-          <Box display='flex' alignItems='baseline' >
-            <Box
-              color='gray.500'
-              fontWeight='semibold'
-              letterSpacing='wide'
-              fontSize='xs'
-              textTransform='uppercase'
-            >
-              <Link href={'https://reddit.com' + post.commentLink} color='link' isExternal>
-                {post.upvoteCount} upvotes &bull; {post.commentCount} comments &bull;  {timeAgoShort(post.created_utc)}
-              </Link>
-            </Box>
-          </Box>
-
+      </Box>
+      <Box p={4} flex={1}>
+        <Box display='flex' alignItems='baseline' >
           <Box
-            mt='1'
+            color='gray.500'
             fontWeight='semibold'
-            as='h4'
-            lineHeight='tight'
-            noOfLines={2}
+            letterSpacing='wide'
+            fontSize='xs'
+            textTransform='uppercase'
           >
-            <Link href={post.url} isExternal>
-              {post.title.replace(/amp;/g,'')}
+            <Link href={'https://reddit.com' + post.commentLink} color='link' isExternal>
+              {post.upvoteCount} upvotes &bull; {post.commentCount} comments &bull;  {timeAgoShort(post.created_utc)}
             </Link>
           </Box>
         </Box>
 
-        <Modal onClose={onClose} isOpen={isOpen} isCentered blockScrollOnMount={false}>
-          <ModalOverlay />
-          <ModalContent maxW='container.xl' maxH='80vh' bg='none' w='auto'>
-          <ModalBody p={0}>
-              <Center >
-                <Box position='relative' width='100%' height='80vh' bg='#222'>
-                  <ReactPlayer url={post.rpan_video.hls_url} width='100%' height='100%' controls stopOnUnmount={false} playing/>
-                  
-                  <ModalCloseButton />
-                </Box>
-              </Center>
-            </ModalBody>
-          </ModalContent>
-        </Modal>
+        <Box
+          mt='1'
+          fontWeight='semibold'
+          as='h4'
+          lineHeight='tight'
+          noOfLines={2}
+        >
+          <Link href={post.url} isExternal>
+            {post.title.replace(/amp;/g,'')}
+          </Link>
+        </Box>
       </Box>
+
+      <Modal onClose={onClose} isOpen={isOpen} isCentered blockScrollOnMount={false}>
+        <ModalOverlay />
+        <ModalContent maxW='container.xl' maxH='80vh' bg='none' w='auto'>
+        <ModalBody p={0}>
+            <Center >
+              <Box position='relative' width='100%' height='80vh' bg='#222'>
+                <ReactPlayer url={post.rpan_video.hls_url} width='100%' height='100%' controls stopOnUnmount={false} playing/>
+                
+                <ModalCloseButton />
+              </Box>
+            </Center>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+      
     </Box>
   )
   else return (
