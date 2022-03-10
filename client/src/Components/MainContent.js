360c686d6cccea2ffe4f239b30b7e2e0601e0f12
bsord <brandon.sorgdrager@gmail.com>
2022-03-09 21:50:23 -0600

    Prevent queries when app not visibile

--- client/src/Components/MainContent.js
+++ client/src/Components/MainContent.js
@@ -14,6 +14,8 @@ import { ViewModeContext } from '../Contexts/ViewModeContext'
 import { LoadingContext } from '../Contexts/LoadingContext'
 import GridView from './GridView'
 import ListView from './ListView'
+import { usePageVisibility } from 'react-page-visibility';
+
 
 let apiEndpoint = process.env.REACT_APP_API ? process.env.REACT_APP_API : window.REACT_APP_API
 apiEndpoint = apiEndpoint.replace(/\/$/, '')
@@ -40,6 +42,7 @@ const findLatestFetch = (posts) => {
 }
 
 const PostView = () => {
+  const isVisible = usePageVisibility()
   const { refreshInterval } = useContext(RefreshIntervalContext)
   const { subreddit } = useContext(SubredditContext)
   const { viewMode } = useContext(ViewModeContext)
@@ -52,37 +55,41 @@ const PostView = () => {
   const maxW = useBreakpointValue({base: 'container.xl', sm: 'container.xl', md: 'container.xl', xl: 'container.xl', '2xl': '1600px'})
 
   const fetchPosts = () => {
-    setLoading(true)
-    fetch(apiEndpoint + '/posts/' + subreddit.replace(/\+/g, '%2b'))
-    .then((response) => response.json())
-    .then((json) => {
-      setPosts(json.data)
-      setTimeout(() => {setLoading(false)}, 1700);
-      setError({show:false})
+    
+      setLoading(true)
+      fetch(apiEndpoint + '/posts/' + subreddit.replace(/\+/g, '%2b'))
+      .then((response) => response.json())
+      .then((json) => {
+        setPosts(json.data)
+        setTimeout(() => {setLoading(false)}, 1700);
+        setError({show:false})
 
-      if(gtThan5MinsAgo(findLatestFetch(json.data))){
-        console.log('delayed')
-        setError({level:"warning", show: true, title: "Delayed Posts:", message: "Content may be out of date.. This may be due to Reddit's API experiencing issues."})
-      }
+        if(gtThan5MinsAgo(findLatestFetch(json.data))){
+          console.log('delayed')
+          setError({level:"warning", show: true, title: "Delayed Posts:", message: "Content may be out of date.. This may be due to Reddit's API experiencing issues."})
+        }
 
-    })
-    .catch((error) => {
-      setError({level:"error", show: true, title: "Ooops:", message: "There was a problem fetching new posts. Retrying.."})
-      setLoading(false)
-    });;
+      })
+      .catch((error) => {
+        setError({level:"error", show: true, title: "Ooops:", message: "There was a problem fetching new posts. Retrying.."})
+        setLoading(false)
+      });;
+    
   }
   // call every interval to api
   useEffect(() => {
     if (refreshInterval && refreshInterval > 0) {
-      const interval = setInterval(fetchPosts, refreshInterval*1000);
-      return () => clearInterval(interval);
+      if(isVisible){
+        const interval = setInterval(fetchPosts, refreshInterval*1000);
+        return () => clearInterval(interval);
+      }
     }
-  }, [refreshInterval, subreddit]); // eslint-disable-line react-hooks/exhaustive-deps
+  }, [refreshInterval, subreddit, isVisible]); // eslint-disable-line react-hooks/exhaustive-deps
 
   // call only on load/refresh
   useEffect(() => {
     fetchPosts()
-  }, [subreddit]); // eslint-disable-line react-hooks/exhaustive-deps
+  }, [subreddit, isVisible]); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
     <Container maxW={maxW} mt={16} pl={padding} pr={padding}>
