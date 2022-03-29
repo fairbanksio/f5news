9229de266998033da91b3d586ba096a68e6c0f2c
bsord <brandon.sorgdrager@gmail.com>
2022-03-29 12:25:46 -0500

    Add matchMedia mock to fix tests

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
+import { screen } from '@testing-library/react';
 import { render } from './test-utils';
 import App from './App';
+import { ColorModeScript } from '@chakra-ui/react';
+const { matchMedia, setMedia } = require("mock-match-media");
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: matchMedia,
+});
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<StrictMode>
+    <ColorModeScript />
+    <App />
+  </StrictMode>);
+  const linkElement = screen.getByText(/F5 News/i);
+  expect(linkElement).toBeInTheDocument();
 });
