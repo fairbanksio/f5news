0d8503e1840e2e9d29c512b04eb8937560c9ac45
bsord <brandon.sorgdrager@gmail.com>
2022-08-10 20:12:06 -0500

    Add additional Post properties to Post List UI
    Signed-off-by: bsord <brandon.sorgdrager@gmail.com>

--- android/app/src/main/java/io/fairbanks/f5oclock/MainActivity.kt
+++ android/app/src/main/java/io/fairbanks/f5oclock/MainActivity.kt
@@ -6,16 +6,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 import androidx.hilt.navigation.compose.hiltViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import de.jensklingenberg.jetpackcomposeplayground.mysamples.github.material.dropdown.SubredditDropdown
 import io.fairbanks.f5oclock.feature_post.presentation.PostItem
 import io.fairbanks.f5oclock.feature_post.presentation.PostViewModel
 import kotlinx.coroutines.flow.collectLatest
@@ -25,6 +29,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+
             val viewModel: PostViewModel = hiltViewModel()
             val state = viewModel.state.value
             val scaffoldState = rememberScaffoldState()
@@ -50,36 +55,48 @@ class MainActivity : ComponentActivity() {
                         Column (
                             modifier = Modifier
                                 .fillMaxSize()
-                                .padding(16.dp)
                         ){
-                            TextField(
-                                value = viewModel.sub.value,
-                                onValueChange = viewModel::onSelectSub,
-                                modifier = Modifier.fillMaxWidth(),
-                                placeholder = {
-                                    Text(text = "Sub..")
-                                }
+
+                            SubredditDropdown(
+                                items = listOf(
+                                    //eventually get this from API via baseurl/subreddits
+                                    "politics",
+                                    "all",
+                                    "news",
+                                    "worldnews",
+                                    "UkrainianConflict",
+                                    "CryptoCurrency"
+                                ),
+                                selected = viewModel.sub.value,
+                                onSubredditSelect = viewModel::onSelectSub
                             )
-                            Spacer(
-                                modifier = Modifier.height(16.dp)
-                            )
+
                             LazyColumn(
                                 modifier = Modifier.fillMaxSize()
+                                    .padding(10.dp)
 
                             ) {
                                 items(state.posts.size){ i ->
                                     val post = state.posts[i]
                                     if(i > 0){
                                         Spacer(
-                                            modifier = Modifier.height(8.dp)
+                                            modifier = Modifier.height(10.dp)
                                         )
                                     }
-                                    PostItem(post = post, modifier = Modifier.padding(4.dp))
+
+                                    PostItem(post = post)
+
                                     if(i < state.posts.size -1){
+                                        Spacer(
+                                            modifier = Modifier.height(10.dp)
+                                        )
                                         Divider()
                                     }
                                 }
                             }
+
+
+
                         }
                     }
                 }
---
+++ android/app/src/main/java/io/fairbanks/f5oclock/feature_post/presentation/SubredditDropdown.kt
@@ -0,0 +1,66 @@
+package de.jensklingenberg.jetpackcomposeplayground.mysamples.github.material.dropdown
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SubredditDropdown(
+    items: List<String>,
+    selected: String,
+    onSubredditSelect: (String) -> Any
+) {
+
+    var expanded by remember { mutableStateOf(false) }
+    val SelectedValue = selected
+    var selectedIndex by remember { mutableStateOf(0) }
+
+    Box(modifier = Modifier
+        .wrapContentSize(Alignment.TopStart)
+    ) {
+
+        Text(
+            items[selectedIndex] + " \\/",
+            modifier = Modifier
+                .fillMaxWidth()
+                .clickable(
+                    onClick = { expanded = true }
+                )
+                .background(Color.Gray)
+                .padding(16.dp)
+        )
+
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false },
+            modifier = Modifier
+                .background(Color.LightGray)
+
+        ) {
+            items.forEachIndexed { index, s ->
+                DropdownMenuItem(onClick = {
+                    onSubredditSelect(s)
+                    selectedIndex = index
+                    expanded = false
+                }) {
+                   if (s == SelectedValue) {
+                       Text(text = s, fontWeight = FontWeight.Bold)
+                    } else {
+                       Text(text = s )
+                    }
+
+                }
+            }
+        }
+    }
+}
