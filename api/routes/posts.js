57366edc556f2f107c5ce2128f12254f06fa9c61
bsord <brandon.sorgdrager@gmail.com>
2023-05-14 16:28:46 -0500

    Add route for just titles

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getPosts, getPostsBySubreddit } = require('../controllers/post');
+const { getPosts, getPostsBySubreddit, getPostTitlesBySubreddit } = require('../controllers/post');
 
 const router = express.Router();
 
@@ -9,5 +9,9 @@ router
 router
   .route('/posts/:sub')
   .get(getPostsBySubreddit);
+
+router
+  .route('/posts/:sub/titles')
+  .get(getPostTitlesBySubreddit);
 
 module.exports = router;
