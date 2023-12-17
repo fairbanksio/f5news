7630ea2159b0ef5f4a2eac234293ebed90e0bfd5
bsord <brandon.sorgdrager@gmail.com>
2023-12-17 12:22:44 -0600

    update user agent

--- scraper/index.js
+++ scraper/index.js
@@ -121,7 +121,7 @@ const fetchPosts = () => {
     timeout: 4000,
     headers: {
       "User-Agent":
-        "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
     },
   })
     .then(parseHtmlJson)
