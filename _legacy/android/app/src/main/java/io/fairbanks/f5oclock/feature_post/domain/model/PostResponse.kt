4ed22c3f358fdd6b2d701ca8295c6bc3d3e7e4d3
Jon Fairbanks <fairplay89@gmail.com>
2024-03-13 15:22:15 -0700

    Remove _legacy files

--- _legacy/android/app/src/main/java/io/fairbanks/f5oclock/feature_post/domain/model/PostResponse.kt
+++
@@ -1,7 +0,0 @@
-package io.fairbanks.f5oclock.feature_post.domain.model
-
-data class PostResponse(
-    val posts:List<Post>,
-    val count: Int,
-    val success: Boolean
-)
--- _legacy/android/app/src/main/java/io/fairbanks/f5oclock/feature_post/domain/repository/PostRepository.kt
+++
@@ -1,9 +0,0 @@
-package io.fairbanks.f5oclock.feature_post.domain.repository
-
-import io.fairbanks.f5oclock.core.util.Resource
-import io.fairbanks.f5oclock.feature_post.domain.model.Post
-import kotlinx.coroutines.flow.Flow
-
-interface PostRepository {
-    fun getPosts(sub: String): Flow<Resource<List<Post>>>
-}
